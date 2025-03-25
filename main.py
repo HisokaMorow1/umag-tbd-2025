a2bcc67eae151bc08cfd98f088e0b21fb235b657
duvan <dufiguer@umag.cl>
2025-03-25 15:47:54 -0300

    cosas nuevas

---
+++ main.py
@@ -0,0 +1,5 @@
+from mylib.basics import my_test
+from exceptions import crea_contraseña
+
+my_test()
+print(crea_contraseña(10))
---
+++ mylib/basics.py
@@ -0,0 +1,4 @@
+from .basics2 import debug_msg
+
+def my_test():
+    print("Hola mundo!")
